c07a1af624f27fdab4437d982bbbd2875a045743
sharks <classshark9@gmail.com>
2022-05-25 20:33:42 -0400

    class code 5252022

--- src/test/java/sharks/TestRunner.java
+++ src/test/java/sharks/TestRunner.java
@@ -5,7 +5,7 @@ public class TestRunner {
 	
 	@Karate.Test
 	Karate testUsers() {
-		return Karate.run().tags("@getPrimaryAccount").relativeTo(getClass());
+		return Karate.run().tags("@E2E").relativeTo(getClass());
 	}
 
 }
